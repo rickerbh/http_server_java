a94f2ec22f0d3106782ce9bac4809361b1d33a3c
Hamish Rickerby <hamish@simplemachines.com.au>
2016-08-16 12:57:27 +1000

    Rename and refactor argument tests

--- src/test/java/com/hamishrickerby/http_server/AppArgumentsTest.java
+++ src/test/java/com/hamishrickerby/http_server/AppArgumentsTest.java
@@ -6,30 +6,25 @@ import junit.framework.TestCase;
  * Created by rickerbh on 15/08/2016.
  */
 public class AppArgumentsTest extends TestCase {
-    public void testEmptyArgumentsAre0Long() {
+    public void testEmptyInputReturnsValidArguments() {
         String[] inputArgs = new String[0];
         AppArguments args = new AppArguments(inputArgs);
         assertEquals(0, args.size());
     }
 
-    public void testArgumentsParseCorrectly() {
+    public void testArgumentsAreRetrieved() {
         String[] inputArgs = {"-p", "3000", "-d", "hello"};
         AppArguments args = new AppArguments(inputArgs);
         assertEquals("3000", args.get("-p"));
         assertEquals("hello", args.get("-d"));
     }
 
-    public void testIncompleteArgumentsFailToGetRead() {
+    public void testIncompleteAndNotPresentArgumentsFailToGetRead() {
         String[] inputArgs = {"-p", "3000", "-d"};
         AppArguments args = new AppArguments(inputArgs);
         assertEquals("3000", args.get("-p"));
         assertNull(args.get("-d"));
+        assertNull(args.get("i-don't-exist"));
     }
 
-    public void testNotPresentArgumentReturnsNull() {
-        String[] inputArgs = {"-p", "3000"};
-        AppArguments args = new AppArguments(inputArgs);
-        assertEquals("3000", args.get("-p"));
-        assertNull(args.get("-d"));
-    }
 }
