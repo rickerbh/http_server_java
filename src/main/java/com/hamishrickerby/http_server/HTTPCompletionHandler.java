c21f1606f4c49a9a0e851bdf2f2c0987225e792b
Hamish Rickerby <hamish@simplemachines.com.au>
2016-08-16 14:40:54 +1000

    Refactor completion handler
    Factor out completion API details to "similar" levels of abstraction

--- src/main/java/com/hamishrickerby/http_server/HTTPCompletionHandler.java
+++ src/main/java/com/hamishrickerby/http_server/HTTPCompletionHandler.java
@@ -11,22 +11,40 @@ import java.util.concurrent.TimeUnit;
 public class HTTPCompletionHandler implements CompletionHandler<AsynchronousSocketChannel, Void> {
     @Override
     public void completed(AsynchronousSocketChannel ch, Void attachment) {
+        byte[] requestBytes = extractRequestBytes(ch);
+        sendResponse(ch, "HTTP/1.1 200 OK".getBytes());
+        closeChannel(ch);
+    }
+
+    @Override
+    public void failed(Throwable exc, Void attachment) {
+
+    }
+
+    private byte[] extractRequestBytes(AsynchronousSocketChannel ch) {
         ByteBuffer buffer = ByteBuffer.allocate(8192);
+        byte[] requestBytes = null;
         try {
             int bytesRead = ch.read(buffer).get(20, TimeUnit.SECONDS);
-            byte[] requestBytes = new byte[bytesRead];
+            requestBytes = new byte[bytesRead];
 
             if (bytesRead > 0 && buffer.position() > 2) {
                 buffer.flip();
                 buffer.get(requestBytes, 0, bytesRead);
                 buffer.clear();
             }
-            // Response
-            ch.write(ByteBuffer.wrap("HTTP/1.1 200 OK".getBytes()));
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            return requestBytes;
         }
+    }
 
+    private void sendResponse(AsynchronousSocketChannel ch, byte[] responseBytes) {
+        ch.write(ByteBuffer.wrap(responseBytes));
+    }
+
+    private void closeChannel(AsynchronousSocketChannel ch) {
         if (ch.isOpen()) {
             try {
                 ch.close();
@@ -34,11 +52,5 @@ public class HTTPCompletionHandler implements CompletionHandler<AsynchronousSock
                 e.printStackTrace();
             }
         }
-
-    }
-
-    @Override
-    public void failed(Throwable exc, Void attachment) {
-
     }
 }
