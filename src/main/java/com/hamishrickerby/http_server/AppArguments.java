e794650457873f466381f27815784f08d872552a
Hamish Rickerby <hamish@simplemachines.com.au>
2016-08-19 09:14:37 +1000

    Refactor AppArguments to remove subclassing
    No more exposed dependencies on HashMap

--- src/main/java/com/hamishrickerby/http_server/AppArguments.java
+++ src/main/java/com/hamishrickerby/http_server/AppArguments.java
@@ -1,16 +1,32 @@
 package com.hamishrickerby.http_server;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by rickerbh on 15/08/2016.
  */
-public class AppArguments extends HashMap<String, String> {
+public class AppArguments {
+    Map<String, String> arguments;
+
     public AppArguments(String[] arguments) {
+        this.arguments = new HashMap<String, String>();
         for (int i = 0; i + 1 < arguments.length; i = i + 2) {
             String key = arguments[i];
             String value = arguments[i + 1];
-            this.put(key, value);
+            this.arguments.put(key, value);
         }
+    }
+
+    public String getOrDefault(String key, String defaultValue) {
+        return arguments.getOrDefault(key, defaultValue);
+    }
+
+    public String get(String key) {
+        return arguments.get(key);
+    }
+
+    public int size() {
+        return arguments.size();
     }
 }
