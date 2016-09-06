11e422bd37284794376b1edc640d9a521fe3ed9a
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-06 12:35:41 +1000

    Add a class to represent ranges

---
+++ src/test/java/com/hamishrickerby/http_server/RequestRangeTest.java
@@ -0,0 +1,28 @@
+package com.hamishrickerby.http_server;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by rickerbh on 6/09/2016.
+ */
+public class RequestRangeTest extends TestCase {
+
+    public void testRangeWithStartAndEnd() {
+        RequestRange range = new RequestRange("0-6", 10);
+        assertEquals(0, range.getStart());
+        assertEquals(6, range.getEnd());
+    }
+
+    public void testRangeWithStartOnly() {
+        RequestRange range = new RequestRange("3-", 10);
+        assertEquals(7, range.getStart());
+        assertEquals(10, range.getEnd());
+    }
+
+    public void testRangeWithEndOnly() {
+        RequestRange range = new RequestRange("-6", 10);
+        assertEquals(4, range.getStart());
+        assertEquals(10, range.getEnd());
+    }
+
+}
