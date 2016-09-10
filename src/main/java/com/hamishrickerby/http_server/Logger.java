4ede589b6511474b7a05be350e79497643684faa
Hamish Rickerby <hamish@happtic.com>
2016-09-10 10:03:33 +1000

    Add logger interface and implement it

---
+++ src/main/java/com/hamishrickerby/http_server/Logger.java
@@ -0,0 +1,10 @@
+package com.hamishrickerby.http_server;
+
+/**
+ * Created by rickerbh on 10/09/2016.
+ */
+public interface Logger {
+    void log(String message);
+
+    String read();
+}
---
+++ src/main/java/com/hamishrickerby/http_server/MemoryLogger.java
@@ -0,0 +1,23 @@
+package com.hamishrickerby.http_server;
+
+/**
+ * Created by rickerbh on 10/09/2016.
+ */
+public class MemoryLogger implements Logger {
+
+    StringBuilder logs;
+
+    public MemoryLogger() {
+        logs = new StringBuilder();
+    }
+
+    @Override
+    public void log(String message) {
+        logs.append(message).append("\n");
+    }
+
+    @Override
+    public String read() {
+        return logs.toString();
+    }
+}
