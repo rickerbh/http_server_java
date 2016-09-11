383070a2a07799077eedb2d4dc17f7c84327c345
Hamish Rickerby <hamish@happtic.com>
2016-09-11 20:51:37 +1000

    Rename MockResponse to FakeResponse
    And add a configurable body payload

--- src/test/java/com/hamishrickerby/http_server/responses/ResponseTest.java
+++ src/test/java/com/hamishrickerby/http_server/responses/ResponseTest.java
@@ -1,7 +1,7 @@
 package com.hamishrickerby.http_server.responses;
 
 import com.hamishrickerby.http_server.Request;
-import com.hamishrickerby.http_server.mocks.MockResponse;
+import com.hamishrickerby.http_server.mocks.FakeResponse;
 import junit.framework.TestCase;
 
 import static com.hamishrickerby.http_server.helpers.HTTPServerTestUtils.assertResponseCodeEquals;
@@ -12,13 +12,13 @@ import static com.hamishrickerby.http_server.helpers.HTTPServerTestUtils.assertR
  */
 public class ResponseTest extends TestCase {
     public void testSuccessfulRequestResponseWith200() {
-        Response response = new MockResponse(new Request("GET / HTTP/1.1"));
+        Response response = new FakeResponse(new Request("GET / HTTP/1.1"));
         assertResponseCodeEquals("200", response);
         assertResponseReasonEquals("OK", response);
     }
 
     public void testLineBreaksAreCRLFOnHeader() {
-        Response response = new MockResponse(new Request("GET / HTTP/1.1"));
+        Response response = new FakeResponse(new Request("GET / HTTP/1.1"));
         byte[] responseBytes = response.getBytes();
 
         String responseText = new String(responseBytes);
