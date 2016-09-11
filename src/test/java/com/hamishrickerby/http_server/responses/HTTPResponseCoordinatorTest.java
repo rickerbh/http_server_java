eec94017fed3f9cd624b491fe0c5e92a8e37c195
Hamish Rickerby <hamish@happtic.com>
2016-09-11 13:38:29 +1000

    Get response coordinator to pass through auth

--- src/test/java/com/hamishrickerby/http_server/responses/HTTPResponseCoordinatorTest.java
+++ src/test/java/com/hamishrickerby/http_server/responses/HTTPResponseCoordinatorTest.java
@@ -1,6 +1,7 @@
 package com.hamishrickerby.http_server.responses;
 
 import com.hamishrickerby.http_server.*;
+import com.hamishrickerby.http_server.auth.RequestAuthenticator;
 import com.hamishrickerby.http_server.mocks.FakeReader;
 import com.hamishrickerby.http_server.mocks.FakeWriter;
 import junit.framework.TestCase;
@@ -30,8 +31,9 @@ public class HTTPResponseCoordinatorTest extends TestCase {
         assertTrue(logger.read().contains(requestString));
     }
 
-    public void testNullLoggerDoesntCauseException() {
-        ResponseCoordinator handler = new HTTPResponseCoordinator("./src/test/resources", null);
+    public void testNullLoggerOrAuthenticatorDoesntCauseException() {
+        HTTPResponseCoordinator handler = new HTTPResponseCoordinator("./src/test/resources", null);
+        handler.setAuthenticator(null);
         ByteReader reader = new FakeReader("GET / HTTP/1.1");
         FakeWriter writer = new FakeWriter();
         Connection connection = new Connection(reader, writer);
@@ -41,4 +43,25 @@ public class HTTPResponseCoordinatorTest extends TestCase {
             fail("Exception in marshalling response");
         }
     }
+
+    public void testCoordinatorUsesAuthenticator() {
+        HTTPResponseCoordinator handler = new HTTPResponseCoordinator("./src/test/resources", null);
+        RequestAuthenticator authenticator = new RequestAuthenticator();
+        authenticator.addCredentials("user", "pass");
+        authenticator.addRoute("/protected");
+        handler.setAuthenticator(authenticator);
+
+        ByteReader reader = new FakeReader("GET /protected HTTP/1.1\r\nAuthorization: Basic dXNlcjpmYWls\r\n");
+        FakeWriter writer = new FakeWriter();
+        Connection connection = new Connection(reader, writer);
+        handler.marshalResponse(connection);
+        assertTrue(new String(writer.readWrittenBytes()).trim().contains("HTTP/1.1 401 Unauthorized"));
+
+        reader = new FakeReader("GET /protected HTTP/1.1\r\nAuthorization: Basic dXNlcjpwYXNz\r\n");
+        writer = new FakeWriter();
+        connection = new Connection(reader, writer);
+        handler.marshalResponse(connection);
+        assertTrue(new String(writer.readWrittenBytes()).trim().contains("HTTP/1.1 404 Not Found"));
+    }
+
 }
