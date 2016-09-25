65f738955dbb0aaf2742346a3071550a8e50f8da
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-26 08:59:42 +1000

    Add container to store cached patches

---
+++ src/main/java/com/hamishrickerby/http_server/FilePatchCache.java
@@ -0,0 +1,24 @@
+package com.hamishrickerby.http_server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by rickerbh on 26/09/2016.
+ */
+public class FilePatchCache {
+
+    protected Map<String, FilePatch> cache;
+
+    public FilePatchCache() {
+        cache = new HashMap<>();
+    }
+
+    public void store(FilePatch patch) {
+        cache.put(patch.path, patch);
+    }
+
+    public FilePatch retrieve(String path) {
+        return cache.get(path);
+    }
+}
---
+++ src/test/java/com/hamishrickerby/http_server/FilePatchCacheTest.java
@@ -0,0 +1,22 @@
+package com.hamishrickerby.http_server;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by rickerbh on 26/09/2016.
+ */
+public class FilePatchCacheTest extends TestCase {
+
+    public void testFilesCanBeStoredAndRetrieved() {
+        FilePatchCache cache = new FilePatchCache();
+        String path = "/path";
+        FilePatch patch = new FilePatch(path, "etag", "Contents".getBytes());
+        cache.store(patch);
+        assertEquals(patch, cache.retrieve(path));
+    }
+
+    public void testNonExistentFilesGiveNull() {
+        FilePatchCache cache = new FilePatchCache();
+        assertNull(cache.retrieve("/no-exist"));
+    }
+}
