157eed4834633615d716d806c2e27de8f8c696be
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-25 21:15:18 +1000

    Support key retrieval via the form store

--- src/main/java/com/hamishrickerby/http_server/FormStore.java
+++ src/main/java/com/hamishrickerby/http_server/FormStore.java
@@ -1,5 +1,7 @@
 package com.hamishrickerby.http_server;
 
+import java.util.Set;
+
 /**
  * Created by rickerbh on 12/09/2016.
  */
@@ -7,4 +9,6 @@ public interface FormStore {
     String read(String key);
 
     void write(String key, String value);
+
+    Set<String> keys();
 }
--- src/test/java/com/hamishrickerby/http_server/MemoryFormStoreTest.java
+++ src/test/java/com/hamishrickerby/http_server/MemoryFormStoreTest.java
@@ -19,4 +19,18 @@ public class MemoryFormStoreTest extends TestCase {
         FormStore store = new MemoryFormStore();
         assertEquals("", store.read("no-data"));
     }
+
+    public void testStoreReturnsKeys() {
+        FormStore store = new MemoryFormStore();
+        String key1 = "myKey";
+        String value1 = "myValue";
+        String key2 = "myKey";
+        String value2 = "myValue";
+
+        store.write(key1, value1);
+        store.write(key2, value2);
+
+        assertTrue(store.keys().contains(key1));
+        assertTrue(store.keys().contains(key2));
+    }
 }
