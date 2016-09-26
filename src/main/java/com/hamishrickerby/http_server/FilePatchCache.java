fd5068e8d534d94a8f80714463caf34a8b0c89b0
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-26 12:33:15 +1000

    Allow checks for specific contents in the file cache

--- src/main/java/com/hamishrickerby/http_server/FilePatchCache.java
+++ src/main/java/com/hamishrickerby/http_server/FilePatchCache.java
@@ -21,4 +21,8 @@ public class FilePatchCache {
     public FilePatch retrieve(String path) {
         return cache.get(path);
     }
+
+    public boolean contains(String path) {
+        return cache.get(path) != null;
+    }
 }
--- src/test/java/com/hamishrickerby/http_server/FilePatchCacheTest.java
+++ src/test/java/com/hamishrickerby/http_server/FilePatchCacheTest.java
@@ -19,4 +19,13 @@ public class FilePatchCacheTest extends TestCase {
         FilePatchCache cache = new FilePatchCache();
         assertNull(cache.retrieve("/no-exist"));
     }
+
+    public void testFilesCanBeStoredAndTestedForExistence() {
+        FilePatchCache cache = new FilePatchCache();
+        String path = "/path";
+        FilePatch patch = new FilePatch(path, "etag", "Contents".getBytes());
+        cache.store(patch);
+        assertTrue(cache.contains(path));
+        assertFalse(cache.contains("/no-file-here"));
+    }
 }
